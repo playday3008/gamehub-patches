118cd517b1f2cc616102481ab928811c57b3e6d6
PlayDay <18056374+playday3008@users.noreply.github.com>
2026-02-22 18:28:56 +0100

    refactor(all): Match ChangePackageNamePatch from upstream
    Changed provider authority check from startsWith to segment-boundary matching to support authorities with prefixes like "poster.".

--- patches/src/main/kotlin/app/revanced/patches/all/misc/packagename/ChangePackageNamePatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/all/misc/packagename/ChangePackageNamePatch.kt
@@ -1,31 +1,35 @@
 package app.revanced.patches.all.misc.packagename
 
-import app.revanced.patcher.patch.Option
-import app.revanced.patcher.patch.booleanOption
-import app.revanced.patcher.patch.resourcePatch
-import app.revanced.patcher.patch.stringOption
+import app.revanced.patcher.patch.*
+import app.revanced.util.asSequence
 import app.revanced.util.getNode
 import org.w3c.dom.Element
+import java.util.logging.Logger
 
-private lateinit var packageNameOption: Option<String>
+lateinit var packageNameOption: Option<String>
 
 /**
- * Set the package name to use. If this is the first call, the value is set.
- * Any subsequent call will not change the value.
+ * Set the package name to use.
+ * If this is called multiple times, the first call will set the package name.
  *
- * @return The package name option value.
+ * @param fallbackPackageName The package name to use if the user has not already specified a package name.
+ * @return The package name that was set.
+ * @throws OptionException.ValueValidationException If the package name is invalid.
  */
-fun setOrGetFallbackPackageName(newPackageName: String): String {
-    val option = packageNameOption
-    if (option.value == option.default) {
-        option.value = newPackageName
+fun setOrGetFallbackPackageName(fallbackPackageName: String): String {
+    val packageName = packageNameOption.value!!
+
+    return if (packageName == packageNameOption.default) {
+        fallbackPackageName.also { packageNameOption.value = it }
+    } else {
+        packageName
     }
-    return option.value!!
 }
 
 val changePackageNamePatch = resourcePatch(
     name = "Change package name",
-    description = "Appends \".revanced\" to the package name by default.",
+    description = "Appends \".revanced\" to the package name by default. " +
+        "Changing the package name of the app can lead to unexpected issues.",
     use = false,
 ) {
     packageNameOption = stringOption(
@@ -36,65 +40,82 @@ val changePackageNamePatch = resourcePatch(
         description = "The name of the package to rename the app to.",
         required = true,
     ) {
-        it == "Default" || it!!.matches(Regex("^[a-z]\\w*(\\.[a-z]\\w*)+$"))
+        it == "Default" || it!!.matches(Regex("^[a-z]\\w*(\\.[a-z]\\w*)+\$"))
     }
 
     val updatePermissions by booleanOption(
         key = "updatePermissions",
         default = false,
         title = "Update permissions",
-        description = "Updates DYNAMIC_RECEIVER_NOT_EXPORTED_PERMISSION references to the new package name.",
-        required = true,
+        description = "Update compatibility receiver permissions. " +
+            "Enabling this can fix installation errors, but this can also break features in certain apps.",
     )
 
     val updateProviders by booleanOption(
         key = "updateProviders",
         default = false,
         title = "Update providers",
-        description = "Rewrites android:authorities on <provider> elements to the new package name.",
-        required = true,
+        description = "Update provider names declared by the app. " +
+            "Enabling this can fix installation errors, but this can also break features in certain apps.",
     )
 
     finalize {
+        /**
+         * Apps that are confirmed to not work correctly with this patch.
+         * This is not an exhaustive list, and is only the apps with
+         * ReVanced specific patches and are confirmed incompatible with this patch.
+         */
+        val incompatibleAppPackages = setOf(
+            // Cannot log in, settings menu is broken.
+            "com.reddit.frontpage",
+
+            // Patches and installs but crashes on launch.
+            "com.duolingo",
+            "com.twitter.android",
+            "tv.twitch.android.app",
+        )
+
         document("AndroidManifest.xml").use { document ->
             val manifest = document.getNode("manifest") as Element
             val packageName = manifest.getAttribute("package")
 
-            val newPackageName = if (packageNameOption.value == "Default") {
-                "$packageName.revanced"
+            if (incompatibleAppPackages.contains(packageName)) {
+                return@finalize Logger.getLogger(this::class.java.name).severe(
+                    "'$packageName' does not work correctly with \"Change package name\"",
+                )
+            }
+
+            val replacementPackageName = packageNameOption.value
+            val newPackageName = if (replacementPackageName != packageNameOption.default) {
+                replacementPackageName!!
             } else {
-                packageNameOption.value!!
+                "$packageName.revanced"
             }
 
             manifest.setAttribute("package", newPackageName)
 
             if (updatePermissions == true) {
-                val permissionName = "$packageName.DYNAMIC_RECEIVER_NOT_EXPORTED_PERMISSION"
-                val newPermissionName = "$newPackageName.DYNAMIC_RECEIVER_NOT_EXPORTED_PERMISSION"
+                val permissions = manifest.getElementsByTagName("permission").asSequence()
+                val usesPermissions = manifest.getElementsByTagName("uses-permission").asSequence()
 
-                // Update <permission> and <uses-permission> elements.
-                for (tagName in listOf("permission", "uses-permission")) {
-                    val nodes = document.getElementsByTagName(tagName)
-                    for (i in 0 until nodes.length) {
-                        val element = nodes.item(i) as Element
-                        if (element.getAttribute("android:name") == permissionName) {
-                            element.setAttribute("android:name", newPermissionName)
-                        }
-                    }
-                }
+                val receiverNotExported = "DYNAMIC_RECEIVER_NOT_EXPORTED_PERMISSION"
+
+                (permissions + usesPermissions)
+                    .map { it as Element }
+                    .filter { it.getAttribute("android:name") == "$packageName.$receiverNotExported" }
+                    .forEach { it.setAttribute("android:name", "$newPackageName.$receiverNotExported") }
             }
 
             if (updateProviders == true) {
-                val providers = document.getElementsByTagName("provider")
-                for (i in 0 until providers.length) {
-                    val provider = providers.item(i) as Element
+                val providers = manifest.getElementsByTagName("provider").asSequence()
+
+                for (node in providers) {
+                    val provider = node as Element
+
                     val authorities = provider.getAttribute("android:authorities")
-                    if (authorities.contains(packageName)) {
-                        provider.setAttribute(
-                            "android:authorities",
-                            authorities.replace(packageName, newPackageName),
-                        )
-                    }
+                    if ("$packageName." !in authorities && !authorities.endsWith(packageName)) continue
+
+                    provider.setAttribute("android:authorities", authorities.replace(packageName, newPackageName))
                 }
             }
         }
